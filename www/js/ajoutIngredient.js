879b55b69f922fd08254b2e43e25c082d5c3f5ab
Bernard <antoine.bernard@live.fr>
2017-04-05 10:39:40 +0200

    bonjour

---
+++ www/js/ajoutIngredient.js
@@ -0,0 +1,12 @@
+/**
+ * Created by Antoine on 04/04/2017.
+ */
+
+function postForm(){
+
+        var labels = document.getElementsByTagName('LABEL');
+        for(var i = 0; i < labels.length ; i++){
+            console.log(labels[i]);
+        }
+
+}
