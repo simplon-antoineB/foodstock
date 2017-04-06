69c457fb1e67787f8d6296887b644d13dbf9cb83
Bernard <antoine.bernard@live.fr>
2017-04-06 14:24:43 +0200

    salutlesloulous

--- www/js/ajoutIngredient.js
+++ www/js/ajoutIngredient.js
@@ -12,41 +12,75 @@ $(document).ready(function(){
         var produitInfo = {
             'Nom': produits,
             'Quantite': codeBarre,
-            'Poids': poids,
-
+            'Poids': poids
         };
 
-        localStorage.setItem('test', JSON.stringify(produitInfo));
+        var banane = {
+            "Nom": "Banane",
+            "Quantite": "2",
+            "Poids": ""
+        };
+        var pate = {
+            "Nom": "Pâtes",
+            "Quantite": "3",
+            "Poids": "500gr"
+        };
+        var farine = {
+            "Nom": "Farine",
+            "Quantite": "1",
+            "Poids":"500g"
+        };
+        var jambon = {
+            "Nom": "Jambon",
+            "Quantite": "2",
+            "Poids": "90g"
+        };
+        var sauceTomate = {
+            "Nom": "Sauce tomate",
+            "Quantite": "1",
+            "Poids": "500g"
+        };
+        var fromageRape = {
+            "Nom": "fromage râpé",
+            "Quantite": "1",
+            "Poids": "350g"
+        };
+        var champignon = {
+            "Nom": "champignon de paris",
+            "Quantite": "4",
+            "Poids": ""
+        };
+        var olive = {
+            "Nom": "Olives noires",
+            "Quantite": "",
+            "Poids": "50g"
+        };
+        var pateAPizza = {
+            "Nom": "Pâte à pizza",
+            "Quantite": "1",
+            "Poids": "260g"
+        };
+
+
+        localStorage.setItem("banane", JSON.stringify(banane));
+        localStorage.setItem("pate", JSON.stringify(pate));
+        localStorage.setItem("farine", JSON.stringify(farine));
+        localStorage.setItem("jambon", JSON.stringify(jambon));
+        localStorage.setItem("sauceTomate", JSON.stringify(sauceTomate));
+        localStorage.setItem("fromageRape", JSON.stringify(fromageRape));
+        localStorage.setItem("champignon", JSON.stringify(champignon));
+        localStorage.setItem("olive", JSON.stringify(olive));
+        localStorage.setItem("pateAPizza", JSON.stringify(pateAPizza));
+
+        /*localStorage.setItem('test', JSON.stringify(produitInfo));
 
         var test = localStorage.getItem('test');
-        console.log('test : ', JSON.parse(test));
-    })
-});
-
-
-
-/*function onSubmit(form){
-    var data = JSON.stringify($("form").serialize());
-
-    console.log(data);
-    return false;
-}*/
-
-
-/*$(document).ready(function(){
-    $("button").click(function(){
-
-    })
-
-
-})*/
-
-
-/*function postForm(){
-
-        var labels = document.getElementsByTagName('LABEL');
-        for(var i = 0; i < labels.length ; i++){
-            console.log(labels[i]);
+        console.log('test : ', JSON.parse(test)); */
+        window.location.replace("stock.html");
+    });
+    $('#annuler').click(
+        function back(event){
+            window.location.replace("stock.html");
         }
-
-}*/
+    )
+});
--- www/js/stock.js
+++ www/js/stock.js
@@ -15,9 +15,52 @@ function setStock() {
         "Quantite": "3",
         "Poids": "500gr"
     };
+    var farine = {
+        "Nom": "Farine",
+        "Quantite": "1",
+        "Poids":"500g"
+    };
+    var jambon = {
+        "Nom": "Jambon",
+        "Quantite": "2",
+        "Poids": "90g"
+    };
+    var sauceTomate = {
+        "Nom": "Sauce tomate",
+        "Quantite": "1",
+        "Poids": "500g"
+    };
+    var fromageRape = {
+        "Nom": "fromage râpé",
+        "Quantite": "1",
+        "Poids": "350g"
+    };
+    var champignon = {
+        "Nom": "champignon de paris",
+        "Quantite": "4",
+        "Poids": ""
+    };
+    var olive = {
+        "Nom": "Olives noires",
+        "Quantite": "",
+        "Poids": "50g"
+    };
+    var pateAPizza = {
+        "Nom": "Pâte à pizza",
+        "Quantite": "1",
+        "Poids": "260g"
+    };
+
 
     localStorage.setItem("banane", JSON.stringify(banane));
     localStorage.setItem("pate", JSON.stringify(pate));
+    localStorage.setItem("farine", JSON.stringify(farine));
+    localStorage.setItem("jambon", JSON.stringify(jambon));
+    localStorage.setItem("sauceTomate", JSON.stringify(sauceTomate));
+    localStorage.setItem("fromageRape", JSON.stringify(fromageRape));
+    localStorage.setItem("champignon", JSON.stringify(champignon));
+    localStorage.setItem("olive", JSON.stringify(olive));
+    localStorage.setItem("pateAPizza", JSON.stringify(pateAPizza));
 }
 
 function getStock() {
